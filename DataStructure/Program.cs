6c3a85d29d5ed44b075faa6a2c4082f008be4355
TechArt <ajumokoibrahimtemitope@gmail.com>
2022-08-21 15:49:36 +0100

    Add project files.

---
+++ DataStructure/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var myLinked = new LinkedList.LinkedList<int>();
+
+myLinked.AddTail(1);
+myLinked.AddHead(2);
+myLinked.AddTail(3);
+myLinked.AddHead(4);
+myLinked.AddTail(5);
+Console.WriteLine("Before removal");
+myLinked.List();
+Console.WriteLine("After removal");
+myLinked.RemoveHead();
+myLinked.List();
+Console.WriteLine("After tail removal");
+myLinked.RemoveTail();
+myLinked.List();
+
+Console.WriteLine(myLinked.Contains(1));
+//Console.WriteLine(myLinked.Count);
